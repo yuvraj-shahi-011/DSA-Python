d9529571eea0a26f7396198c22a4ebc8c8cd7022
yuvraj-shahi-011 <yuvrajshahi2081@gmail.com>
2026-07-18 17:53:30 +0530

    Sorting: Add Kth Largest Element

---
+++ Sorting/Kth_Largest.py
@@ -0,0 +1,5 @@
+n = int(input("Enter number of elements: "))
+arr = list(map(int, input("Enter elements: ").split()))
+k = int(input("Enter value of k: "))
+arr.sort(reverse=True)
+print(k, "th largest element is:", arr[k - 1])
