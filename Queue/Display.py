465d00981d9e0348c1cc222da3758b289205587e
yuvraj-shahi-011 <yuvrajshahi2081@gmail.com>
2026-08-02 20:35:54 +0530

    Queue: Add Display Queue

---
+++ Queue/Display.py
@@ -0,0 +1,7 @@
+queue = []
+n = int(input("Enter number of elements: "))
+for i in range(n):
+    queue.append(input("Enter element: "))
+print("Queue Elements:")
+for element in queue:
+    print(element)
