2ff219b50cd270091841c18b71281ceb26ecba8c
yuvraj-shahi-011 <yuvrajshahi2081@gmail.com>
2026-09-13 16:26:21 +0530

    Linked List: Add Create and Display

---
+++ Linked_List/Create_Display.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+head = None
+n = int(input("Enter number of nodes: "))
+for i in range(n):
+    data = int(input("Enter element: "))
+    new_node = Node(data)
+    if head is None:
+        head = new_node
+    else:
+        current = head
+        while current.next is not None:
+            current = current.next
+        current.next = new_node
+print("Linked List:")
+current = head
+while current is not None:
+    print(current.data, end=" -> ")
+    current = current.next
+print("None")
